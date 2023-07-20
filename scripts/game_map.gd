edacb20598509b68c52ea95ca9ff852b75caa5f6
davis.trem <davis.trem@gmail.com>
2023-07-20 19:00:24 -0400

    use buttons for neighborhoods

--- scripts/game_map.gd
+++ scripts/game_map.gd
@@ -21,6 +21,12 @@ const Neighborhood = preload('res://scripts/neighborhood.gd')
 @onready var neighborhood_menu_close_button = $NeighborhoodMenu/ColorRect/MarginContainer/VBoxContainer/MarginContainer/CloseButton
 @onready var neighborhood_menu_actions_button = $NeighborhoodMenu/ColorRect/MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/ActionsButton
 @onready var neighborhood_menu_status_label = $NeighborhoodMenu/ColorRect/MarginContainer/VBoxContainer/MarginContainer/StatusLabel
+@onready var trigger_menu = $TriggerMenu
+@onready var trigger_menu_description_label = $TriggerMenu/ColorRect/MarginContainer/VBoxContainer/DescriptionLabel
+@onready var trigger_menu_status_label = $TriggerMenu/ColorRect/MarginContainer/VBoxContainer/StatusUpdateLabel
+@onready var trigger_menu_options_button = $TriggerMenu/ColorRect/MarginContainer/VBoxContainer/OptionsButton
+@onready var trigger_menu_confirm_button = $TriggerMenu/ColorRect/MarginContainer/ConfirmButton
+@onready var event_timer = $EventTimer
 
 @onready var events = Events.new()
 
@@ -66,6 +72,8 @@ func add_player(peer_id):
 	set_multiplayer_authority(peer_id)
 	players[peer_id] = Player.new()
 	players[peer_id].id = peer_id
+	for i in range(territory_count):
+		players[peer_id].territories_respect.append(0)
 	if peer_id == multiplayer.get_unique_id():
 		local_player_multiplayer_unique_id = peer_id
 
@@ -78,7 +86,7 @@ func _process(delta):
 func _draw_territories(size: int = territory_count, save_exist: bool = false):
 	for i in range(size):
 		var neighborhood = neighborhood_scene.instantiate()
-		neighborhood.set_input_handler(_neighborhood_input_handler)
+		neighborhood.connect('pressed', func (): _show_neighborhood_menu(neighborhood))
 		
 		if save_exist:
 			neighborhood.stats = _save_game.neighoborhood_stats_list[i]
@@ -99,13 +107,7 @@ func _draw_territories(size: int = territory_count, save_exist: bool = false):
 		territories.add_child(neighborhood)
 
 
-func _neighborhood_input_handler(event: InputEvent, neighborhood: Neighborhood):
-	if not neighborhood_menu.visible:
-		_show_neighborhood_menu(neighborhood)
-
-
 func _show_neighborhood_menu(neighborhood: Neighborhood):
-	print(neighborhood.get_index())
 	_selected_neighborhood_index = neighborhood.get_index()
 	
 	neighborhood_menu_actions_button.get_popup().set_item_disabled(
@@ -132,7 +134,6 @@ func _close_neighborhood_menu():
 
 func _neighborhood_menu_action_selected(id):
 	var text = neighborhood_menu_actions_button.get_popup().get_item_text(id)
-	print(id, text)
 	match id:
 		NEIGHBORHOOD_ACTIONS.RENT:
 			players[local_player_multiplayer_unique_id].rentals.append(_selected_neighborhood_index)
@@ -141,39 +142,110 @@ func _neighborhood_menu_action_selected(id):
 			neighborhood_menu_status_label.show()
 		NEIGHBORHOOD_ACTIONS.START_BUSINESS:
 			pass
+	trigger_event(players[local_player_multiplayer_unique_id])
 	_save_game.write_savegame()
 
 
-func trigger_event(player: Player, trigger_type):
+func trigger_event(player: Player, trigger_type = null):
+	trigger_menu_description_label.text = ''
+	trigger_menu_status_label.text = ''
+	trigger_menu_options_button.hide()
+	trigger_menu_confirm_button.hide()
+	
 	var applicable_outcomes = [];
 	if trigger_type != null:
 		applicable_outcomes = events.triggers[trigger_type].get('outcomes')
 	else:
 		for type in events.triggers:
 			var condition = events.triggers[type].get('condition')
-			if typeof(condition) == TYPE_STRING and _evaluateString(condition, player):
+			if (
+				not player.past_four_triggers.has(type)
+				and typeof(condition) == TYPE_STRING
+				and _evaluateString(condition, player)
+			):
+				for outcome in events.triggers[type].get('outcomes'):
+					if outcome.get('triggered_by') == null:
+						outcome['triggered_by'] = type
 				applicable_outcomes.append_array(events.triggers[type].get('outcomes'))
+	
+	if applicable_outcomes.size() == 0:
+		return
 	
 	var outcomes = _calculate_outcomes_accumulated_chance(applicable_outcomes, player)
 	var chance = randf_range(0, outcomes[outcomes.size() - 1].get('accumulated_chance'))
 	for outcome in outcomes:
 		if outcome.get('accumulated_chance') > chance:
+			player.past_four_triggers.append(outcome.get('triggered_by'))
+			if (player.past_four_triggers.size() > 4):
+				player.past_four_triggers.pop_front()
+			trigger_menu.show()
 			for stat_update in outcome.get('stat_updates', []):
-				var update_value = (
-					_evaluateString(stat_update.get('value'), player)
-					if typeof(stat_update.get('value')) == TYPE_STRING
-					else stat_update.get('value')
-				)
-				if not (typeof(update_value) == TYPE_BOOL and update_value == false):
-					player.set(stat_update.get('name'), update_value)
+				_handle_stat_updates(stat_update, player)
 			if outcome.get('trigger'):
-				trigger_event(player, outcome.get('trigger'))
+				trigger_menu_confirm_button.connect(
+					'pressed',
+					func (): trigger_event(player, outcome.get('trigger'))
+				)
+				trigger_menu_confirm_button.show()
 			elif outcome.get('event'):
-				# Present Event
-				pass
+				for option in outcome.get('event').get('options'):
+					trigger_menu_options_button.get_popup().add_item(option.get('type'))
+				trigger_menu_options_button.get_popup().connect(
+					'id_pressed',
+					func (id): _trigger_menu_option_selected(
+						id,
+						outcome.get('event').get('options'),
+						player
+					)
+				)
+				trigger_menu_options_button.show()
+			else:
+				trigger_menu_confirm_button.connect(
+					'pressed',
+					func (): trigger_menu.hide()
+				)
+				trigger_menu_confirm_button.show()
 	
 	if _save_game:
 		_save_game.write_savegame()
+
+
+func _handle_stat_updates(stat_update, player: Player):
+	var update_value = (
+		_evaluateString(stat_update.get('value'), player)
+		if typeof(stat_update.get('value')) == TYPE_STRING
+		else stat_update.get('value')
+	)
+	if not (typeof(update_value) == TYPE_BOOL and update_value == false):
+		player.set(stat_update.get('name'), update_value)
+	var new_value = player.get(stat_update.get('name'))
+	trigger_menu_status_label.text += (
+		'You now have {1} {0}\n'.format([stat_update.get('name'), new_value.size()])
+		if typeof(new_value) == TYPE_ARRAY
+		else '{0} is now {1}\n'.format([stat_update.get('name'), new_value])
+	)
+
+
+func _trigger_menu_option_selected(selected_id, options, player):
+	trigger_menu_description_label.text = ''
+	trigger_menu_status_label.text = ''
+	trigger_menu_options_button.hide()
+	trigger_menu_confirm_button.connect(
+		'pressed',
+		func (): trigger_menu.hide()
+	)
+	trigger_menu_confirm_button.show()
+	var text = trigger_menu_options_button.get_popup().get_item_text(selected_id)
+	for option in options:
+		if text == option.get('type'):
+			for stat_update in option.get('stat_updates', []):
+				_handle_stat_updates(stat_update, player)
+			if option.get('trigger'):
+				trigger_menu_confirm_button.connect(
+					'pressed',
+					func (): trigger_event(player, option.get('trigger'))
+				)
+			return
 
 
 func _calculate_outcomes_accumulated_chance(outcomes: Array, player) -> Array:
@@ -202,3 +274,24 @@ func _evaluateString(command: String, player):
 	var ref = RefCounted.new()
 	ref.set_script(script)
 	return ref.eval(player)
+
+
+func _on_event_timer_timeout():
+	var player: Player = players[local_player_multiplayer_unique_id]
+	player.money += player.income
+	for neighborhood_index in player.rentals:
+		player.money -= territories.get_child(neighborhood_index).stats.rent
+	for business in player.businesses:
+		player.money -= territories.get_child(business.territory_index).stats.cost_to_run_business
+		player.money += territories.get_child(business.territory_index).stats.business_payout
+		if business.get('extortioner'):
+			pass
+	
+	if player.rentals.size() == 0:
+		player.sanity -= 0.1
+		if player.sanity < 0:
+			print('player ends')
+	
+	trigger_event(players[local_player_multiplayer_unique_id])
+	event_timer.start()
+	print('agaaain')
--- scripts/neighborhood.gd
+++ scripts/neighborhood.gd
@@ -1,5 +1,5 @@
 class_name Neighborhood
-extends ColorRect
+extends Button
 
 
 const NeighborhoodStats = preload('res://resources/neighborhood_stats.gd')
@@ -9,8 +9,6 @@ const NeighborhoodStats = preload('res://resources/neighborhood_stats.gd')
 
 var stats: NeighborhoodStats
 var initial_status_label_text = ''
-
-var _input_handler: Callable = func (event, n): return
 
 
 # Called when the node enters the scene tree for the first time.
@@ -24,18 +22,4 @@ func _process(delta):
 	pass
 
 
-func _input(event):
-	if event is InputEventScreenTouch:
-		pass
-	if (event is InputEventMouseButton
-		and event.button_index == MOUSE_BUTTON_LEFT
-		and event.is_pressed()
-		and (global_position.x < event.position.x and global_position.y < event.position.y)
-		and (event.position.x < (global_position + size).x and event.position.y < (global_position + size).y)
-	):
-		_input_handler.call(event, self)
-
-
-func set_input_handler(handler: Callable):
-	_input_handler = handler
 
